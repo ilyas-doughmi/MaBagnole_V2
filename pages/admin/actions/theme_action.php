1438b2a82f0ec4cc705a2dac6893794f1f0f1039
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-01-06 14:43:16 +0100

    feat: improve code formatting and structure in theme_action.php

--- pages/admin/actions/theme_action.php
+++ pages/admin/actions/theme_action.php
@@ -6,38 +6,42 @@ $pdo = DB::connect();
 
 $theme = new Theme($pdo);
 
-if($_SERVER["REQUEST_METHOD"] == "POST")
-{
-    if(isset($_POST["add_theme"]))
-    {
-        $theme->__set("name",$_POST["title"]);
-        $theme->__set("media",$_POST["image"]);
-        $theme->__set("description",$_POST["description"]);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["add_theme"])) {
+        $theme->__set("name", $_POST["title"]);
+        $theme->__set("media", $_POST["image"]);
+        $theme->__set("description", $_POST["description"]);
 
-        if($theme->createTheme())
-        {
+        if ($theme->createTheme()) {
             header("location: ../themes.php?msg=all_good");
             exit();
-        }
-        else{
+        } else {
             header("location: ../themes.php?msg=problem");
             exit();
         }
     }
 
-    if(isset($_POST["edit_theme"]))
-    {
-        $theme->__set("name",$_POST["title"]);
-        $theme->__set("media",$_POST["image"]);
-        $theme->__set("description",$_POST["description"]);
-        $theme->__set("themeId",$_POST["theme_id"]);
+    if (isset($_POST["edit_theme"])) {
+        $theme->__set("name", $_POST["title"]);
+        $theme->__set("media", $_POST["image"]);
+        $theme->__set("description", $_POST["description"]);
+        $theme->__set("themeId", $_POST["theme_id"]);
 
-        if($theme->editTheme())
-        {
+        if ($theme->editTheme()) {
             header("location: ../themes.php?msg=allgood");
             exit();
+        } else {
+            header("location: ../themes.php?msg=bad");
+            exit();
         }
-        else{
+    }
+
+    if (isset($_POST["delete_theme"])) {
+        $theme->__set("themeId", $_POST["theme_id"]);
+        if ($theme->deleteTheme()) {
+            header("location: ../themes.php?msg=allgood");
+            exit();
+        } else {
             header("location: ../themes.php?msg=bad");
             exit();
         }
