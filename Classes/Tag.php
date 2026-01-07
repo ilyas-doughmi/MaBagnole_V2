560c33574f748dafc88aec5640495154e079bdf9
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-01-07 12:37:11 +0100

    feat: add new methodes to add tag and remove tag and get all tags we have

--- Classes/Tag.php
+++ Classes/Tag.php
@@ -20,4 +20,40 @@ class tag{
     {
         return $this->$name;
     }
+
+    public function getTags()
+    {
+        $query = "SELECT * FROM tag";
+        $stmt = $this->pdo->prepare($query);
+        try{
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }catch(PDOException $e){
+            return false;
+        }
+    }
+
+    public function addTag($name)
+    {
+        $query = "INSERT INTO tag (name) VALUES (:name)";
+        $stmt = $this->pdo->prepare($query);
+        try{
+            $stmt->execute([':name' => $name]);
+            return true;
+        }catch(PDOException $e){
+            return false;
+        }
+    }
+
+    public function deleteTag($id)
+    {
+        $query = "DELETE FROM tag WHERE tagId = :id";
+        $stmt = $this->pdo->prepare($query);
+        try{
+            $stmt->execute([':id' => $id]);
+            return true;
+        }catch(PDOException $e){
+            return false;
+        }
+    }
 }
