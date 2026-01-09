ebc3223868c70840c9d809171e9f46b692c14c6e
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-01-09 12:33:36 +0100

    feat: add new action file to get data from blog detail and send it to class to remove comment

---
+++ pages/delete_comment.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+require_once '../includes/guard.php';
+require_once '../Classes/db.php';
+require_once '../Classes/Comment.php';
+
+if (!isset($_SESSION['id'])) {
+    header("Location: login.php");
+    exit();
+}
+
+if (isset($_GET['id']) && isset($_GET['article'])) {
+    $db = DB::connect();
+    $commentObj = new Comment($db);
+    
+    $comment_id = $_GET['id'];
+    $article_id = $_GET['article'];
+    $user_id = $_SESSION["id"];
+    
+    // Attempt to delete the comment (checks user ownership in the query)
+    if ($commentObj->deleteComment($comment_id, $user_id)) {
+        header("Location: blog-details.php?article=" . $article_id . "&msg=comment_deleted");
+    } else {
+        header("Location: blog-details.php?article=" . $article_id . "&error=delete_failed");
+    }
+    exit();
+} else {
+    header("Location: blog.php");
+    exit();
+}
