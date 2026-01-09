00fb54cf3738200d7e6bd125bf59d3a9977f9777
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-01-09 11:59:45 +0100

    feat: adding new methode to add new comment

--- Classes/Comment.php
+++ Classes/Comment.php
@@ -18,4 +18,20 @@ class Comment {
     public function __get($name) {
         return $this->$name;
     }
+
+    public function addComment() {
+        $query = "INSERT INTO comments (content, user_id, article_id) 
+                  VALUES (:content, :user_id, :article_id)";
+        $stmt = $this->pdo->prepare($query);
+        try {
+            $stmt->execute([
+                ':content' => $this->content,
+                ':user_id' => $this->user_id,
+                ':article_id' => $this->article_id
+            ]);
+            return true;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
 }
